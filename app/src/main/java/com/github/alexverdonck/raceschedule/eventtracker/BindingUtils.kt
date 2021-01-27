346080b0e60ae9640760ed2ad0fc18b3461e3ad5
alexverdonck <208368x@student.swin.edu.au>
2021-01-27 18:16:14 +1100

    Formatted session times

--- app/src/main/java/com/github/alexverdonck/raceschedule/eventtracker/BindingUtils.kt
+++ app/src/main/java/com/github/alexverdonck/raceschedule/eventtracker/BindingUtils.kt
@@ -3,8 +3,11 @@ package com.github.alexverdonck.raceschedule.eventtracker
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.github.alexverdonck.raceschedule.data.Event
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
+
+val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("d MMM yyyy h:mm a")
 
 @BindingAdapter("eventName")
 fun TextView.setEventName(item: Event?) {
@@ -23,13 +26,13 @@ fun TextView.setEventLocation(item: Event?) {
 @BindingAdapter("eventRaceTime")
 fun TextView.setEventTime(item: Event?) {
     item?.let {
-        text = item.sessions["Race"].toString()// need to format this for time/countdown/live/localtime stuff
+        text = item.sessions["Race"]?.format(formatter.withZone(ZoneId.systemDefault()))// need to format this for time/countdown/live/localtime stuff
     }
 }
 
 @BindingAdapter("eventSession")
 fun TextView.setEventSession(item: String?) {
     item?.let {
-        text = item// change, need to display each session
+        text = item
     }
 }
